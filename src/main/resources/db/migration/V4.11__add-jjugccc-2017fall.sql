956e20b0b0c300879985e3655e29465267c8e482
Toshiaki Maki <tmaki@pivotal.io>
2017-11-17 01:38:59 +0900

    Create V4.11__add-jjugccc-2017fall.sql

---
+++ src/main/resources/db/migration/V4.11__add-jjugccc-2017fall.sql
@@ -0,0 +1,132 @@
+INSERT INTO seminar(seminar_id, seminar_date, seminar_name) VALUES (0x00000000000000000000000020171118, '2017-11-18', 'JJUG CCC 2017 FALL');
+
+-- CFP
+
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x7ef869cec1ea4109b92d5dc2161216ec, 'a01. AsciiDocとPlantUMLでドキュメント作成', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x7d150876baa148ada48cd65beb60e900, 'a02. サンプルアプリケーションで学ぶApache Cassandraを使ったJavaアプリケーションの作り方', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x228b3e2bb2a6492894753484a3deb63f, 'a07. Java でつくる本格形態素解析器', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x6607caaeba854ec287534f17b6fdda72, 'a08. 【Serverspec】公共案件におけるWeblogicのテスト事例', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0xb9af052e6e984750a925996bc9ac36fb, 'c01. Selenide or Geb？ あなたはそのときどちらを使う？', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0xcda2de8a69414e99843ef39ce64f6c12, 'c02. Apache Camel + hawtio + Spring Boot によるモダンなインテグレーション・マイクロサービス', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x292485bc920a4795aae1d3018ef9a90d, 'c05. オレオレJVM言語を作ってみる（四則演算するだけだけど）', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0xd9f6380bbba54353a25f904851266d80, 'c06. Effects of SHAttered', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0xb36e0a31536e4d728d31e7cbc3a49a88, 'c07. CPUから見たG1GC', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0xe7973185a30740c786ca332c3b31d5f3, 'e06. Spring BootとKafkaでCQRSなアプリを動かしてみる', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x3465bb96d395442da3b2dc7e5d7ca0b3, 'g01. Backlog: Java から Scala への移行の歩み', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0xbdbc89709d7a43538b31e67874c6ee81, 'g03. Java 9を迎えた今こそ！Java本格(再)入門', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x9c14a835f8934be7b2316c601c56f2c2, 'g04. Spring Security にできること・できないこと', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0xce1c4174eb734931ae388df95def1f27, 'g07. 最近のDeep Learning事情とJava', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x3fcde47eb4db4a039017a63996dabad8, 'i03. Elastic 6.0 ハンズオン', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0xf9f137542b6a48df852a1fcbe5ce48be, 'l01. SpringBootとMyBatisでデータベースを可視化する', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x669473517388409bb7b6b1f31ef1b3c8, 'l02. JVM上で動くPython処理系実装のススメ', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x55089770223441b986aa9a349598f9b3, 'm01. サーバーサイドでの非同期処理で色々試したよ。', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0xdab51d2713bc45128bd3161a12916164, 'm02. ユニットテストのアサーション 流れるようなインターフェースのAssertJを添えて 入門者仕立て', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0xb7dcc9dec3b4492aa063bcc6a228cddb, 'm03. 次の一歩を踏み出そう！ ーOCJPの知識でちょっといいJavaコーディングー', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x7f87da74fc754ddfbce7c93ea2c1d24c, 'm04. DDD × CQRS - コマンドとクエリでORMを使い分けた話', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x65a0f91bc67f4a2d8eb6de3da7086c20, 'm05. 劇的改善 CI４時間から５分へ〜私がやった１０のこと〜', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x5392c2c5a10945c6bf07ebd54503fb6e, 'm06. Docker ではじめる Java EE アプリケーション開発', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0xf2e9518491e9411bbd4caef49b149b4d, 'm07. Java 8 Stream APIの代わりにEclipse Collectionsを使って開発してみた', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0xda7388fd2221473c897b540aff69e358, 'm08. 速いソートアルゴリズムを書こう！！', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x7a9d7ca9d4b84b338cb55344cba28bde, 'm09. Design Pattern in Presto source code', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0xd1181652af074e9ab2ae30f6e3e08bf8, 'm10. Javaで使えるもう一つのコンパイル方式 - AOT', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x780098d80a424a338763494c60a0e487, 'm11. DBのTCPプロトコルとJDBC', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x7f724cb5876342e9bac38a415d60ed34, 'm12. Java x ArduinoのIoT アーキテクチャパターン', 0x00000000000000000000000020171118);
+
+--
+
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x228b3e2bb2a6492894753484a3deb63f, '高岡 一馬');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x292485bc920a4795aae1d3018ef9a90d, '阪田 浩一');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x3465bb96d395442da3b2dc7e5d7ca0b3, '谷本陽介 & 松本裕二');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x3fcde47eb4db4a039017a63996dabad8, '樋口 慎');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x5392c2c5a10945c6bf07ebd54503fb6e, '齋藤　耕平');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x55089770223441b986aa9a349598f9b3, 'Koji Lin');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x65a0f91bc67f4a2d8eb6de3da7086c20, 'aha-oretama');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x6607caaeba854ec287534f17b6fdda72, '佐々木 優太朗');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x669473517388409bb7b6b1f31ef1b3c8, '澁谷 典明(Yoshiaki Shibutani)');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x780098d80a424a338763494c60a0e487, 'yohei yamana');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x7a9d7ca9d4b84b338cb55344cba28bde, 'Shin So（曾臻）');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x7d150876baa148ada48cd65beb60e900, '森下 雄貴');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x7ef869cec1ea4109b92d5dc2161216ec, '梅澤 雄一郎');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x7f724cb5876342e9bac38a415d60ed34, 'Yusuke Yamamoto');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x7f87da74fc754ddfbce7c93ea2c1d24c, '松岡 幸一郎');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x9c14a835f8934be7b2316c601c56f2c2, 'opengl-8080');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0xb36e0a31536e4d728d31e7cbc3a49a88, '数村憲治');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0xb7dcc9dec3b4492aa063bcc6a228cddb, 'ぜろゆ');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0xb9af052e6e984750a925996bc9ac36fb, '島根 義和 & 高橋 陽太郎');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0xbdbc89709d7a43538b31e67874c6ee81, '岡田 拓也');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0xcda2de8a69414e99843ef39ce64f6c12, '佐藤 匡剛');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0xce1c4174eb734931ae388df95def1f27, 'EVGENIY(髙橋良貴)');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0xd1181652af074e9ab2ae30f6e3e08bf8, '西川 彰広');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0xd9f6380bbba54353a25f904851266d80, 'Michael Demey');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0xda7388fd2221473c897b540aff69e358, '松原 正和');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0xdab51d2713bc45128bd3161a12916164, '内立良介（うちたて　りょうすけ）');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0xe7973185a30740c786ca332c3b31d5f3, '椎葉 光行');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0xf2e9518491e9411bbd4caef49b149b4d, 'Nagahori Shota');
+
+--
+
+INSERT INTO session_speakers (session_id, speaker) VALUES (0x228b3e2bb2a6492894753484a3deb63f, 'kazuma-t');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0x292485bc920a4795aae1d3018ef9a90d, 'jyukutyo');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0x3465bb96d395442da3b2dc7e5d7ca0b3, 'safx');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0x3fcde47eb4db4a039017a63996dabad8, 'higu0shin');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0x5392c2c5a10945c6bf07ebd54503fb6e, 'sightseeker');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0x55089770223441b986aa9a349598f9b3, 'kojilin');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0x65a0f91bc67f4a2d8eb6de3da7086c20, 'aha-oretama');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0x6607caaeba854ec287534f17b6fdda72, 'keydaimon');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0x669473517388409bb7b6b1f31ef1b3c8, 'yotchang4s');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0x780098d80a424a338763494c60a0e487, 'vertical-blank');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0x7a9d7ca9d4b84b338cb55344cba28bde, 'zz22394');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0x7d150876baa148ada48cd65beb60e900, 'yukim');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0x7ef869cec1ea4109b92d5dc2161216ec, 'garbagetown');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0x7f724cb5876342e9bac38a415d60ed34, 'yusuke');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0x7f87da74fc754ddfbce7c93ea2c1d24c, 'little-hands');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0x9c14a835f8934be7b2316c601c56f2c2, 'opengl-8080');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0xb36e0a31536e4d728d31e7cbc3a49a88, 'kazumura');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0xb7dcc9dec3b4492aa063bcc6a228cddb, 'zer0-u');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0xb9af052e6e984750a925996bc9ac36fb, 'PoohSunny');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0xbdbc89709d7a43538b31e67874c6ee81, 'takuyao');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0xcda2de8a69414e99843ef39ce64f6c12, 'tadayosi');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0xce1c4174eb734931ae388df95def1f27, 'yo-takahashi');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0xd1181652af074e9ab2ae30f6e3e08bf8, 'anishi1222');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0xd9f6380bbba54353a25f904851266d80, 'MyMilkedEek');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0xda7388fd2221473c897b540aff69e358, 'm-matsubara');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0xdab51d2713bc45128bd3161a12916164, 'b1a9id');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0xe7973185a30740c786ca332c3b31d5f3, 'bufferings');
+INSERT INTO session_speakers (session_id, speaker) VALUES (0xf2e9518491e9411bbd4caef49b149b4d, 'nashcft');
+
+
+--
+
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x7b30cb8a39d646f58b426df776a37f2a, 'a03. サーバサイドKotlin', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x1da6a8e64fff454fac551abc17361bdb, 'a04. ヤフー発のメッセージキュー「Pulsar」のご紹介', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0xf9dc56a1a54a44e4bb98ff109179a488, 'a05. JDKの新しいリリースモデル', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x6cad358b2ac24542aa69ad2d9f88fdba, 'a06. ついに来たリアルタイムSpark〜ビッグデータ処理の新常識・SnappyDataの実力〜', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x392d66e07a3f4519b6e209506235c22e, 'c03. Open Liberty:オープンソースになったWebSphere Liberty', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x5446b4c7fd974d21ac69afa12930c322, 'c04. ゴールドマン・サックスにおけるCamundaを用いたビジネスプロセスの可視化とワークフローの自動化', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x7e7b3febdcf546b7b9fb03ecd0ab048f, 'e01. 年齢も経験も関係ない!ステップアップするためのJavaコミュニティ活用術', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0xa41c57a102c14a1f9330ae382f964d53, 'e02. OpenJDK 参加入門', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x4158ba049fb446098148416801d35acb, 'e03. Pivotal認定講師が徹底解説!Spring Bootの本当の理解ポイント', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x965c6d40365246749d7ee9b1d5606717, 'e04. Java SE 9のご紹介', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x901c71dfbd0244da8d535d18179a332a, 'e05. 入社してから運用しているサービスの運用改善奮闘記', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0xb8a81d70eaf64d61afb59e9a667baf53, 'g02. 10年前のレガシーシステムをサーバーサイドKotlinでフルリニューアルした話', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x17bc313215a94c7a85180dca7ffa891f, 'g05. Javaが「書ける」から「できる」になれる!メモリ節約ノウハウ話', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x91fec581e12543c0883a2c8395a4a039, 'g06. 新しいプログラミング言語の学び方〜HTTPサーバーを作って学ぶJava, Scala, Clojure〜', 0x00000000000000000000000020171118);
+INSERT INTO session (session_id, session_name, seminar_id) VALUES (0x73626921283d4c56ade071d60ccf122d, 'i01. モブプロで実施する Cognitive Service & Botハッカソン', 0x00000000000000000000000020171118);
+
+--
+
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x7b30cb8a39d646f58b426df776a37f2a, '大谷弘喜');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x1da6a8e64fff454fac551abc17361bdb, '坂本雅宏');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0xf9dc56a1a54a44e4bb98ff109179a488, '伊藤敬');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x6cad358b2ac24542aa69ad2d9f88fdba, '山河征紀');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x392d66e07a3f4519b6e209506235c22e, '田中孝清');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x5446b4c7fd974d21ac69afa12930c322, '福井達也,石井すみれ');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x7e7b3febdcf546b7b9fb03ecd0ab048f, '横田紋奈');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0xa41c57a102c14a1f9330ae382f964d53, 'David Buck');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x4158ba049fb446098148416801d35acb, '多田真敏');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x965c6d40365246749d7ee9b1d5606717, '宮川 拓');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x901c71dfbd0244da8d535d18179a332a, '佐藤彗太');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0xb8a81d70eaf64d61afb59e9a667baf53, '前原秀徳');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x17bc313215a94c7a85180dca7ffa891f, '猪鼻哲也');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x91fec581e12543c0883a2c8395a4a039, '田所駿佑');
+INSERT INTO session_speaker_display_names (session_id, speaker_display_name) VALUES (0x73626921283d4c56ade071d60ccf122d, 'Microsoft');
